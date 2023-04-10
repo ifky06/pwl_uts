124174a18985bce4eb1f1f20951d6fbd000d003a
Ahmad Rifki Fauzi <ahmadrifki3403@gmail.com>
2023-04-10 10:09:23 +0700

    add songs model msc

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.2.0
     </div>
-    <strong>Copyright &copy; 2023 Siapa.
+      <strong>Copyright &copy; 2023 Siapa.</strong>
   </footer>
